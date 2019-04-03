d1393429fc08389062a3e4ee49ef6c64eaabdd2d
qinhao101 <qinhao.101@163.com>
2019-04-03 13:43:16 +0800

    zmr-test1

---
+++ blog/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category, Tag
+import markdown
+from comments.forms import CommentForm
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# Create your views here.
+
+
+# def index(request):
+#     post_list = Post.objects.all().order_by('-create_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     post.increase_views()
+#     post.body = markdown.markdown(post.body,
+#                                   extensions=[
+#                                       'markdown.extensions.extra',
+#                                       'markdown.extensions.codehilite',
+#                                       'markdown.extensions.toc',
+#                                   ])
+#     form = CommentForm()
+#     comment_list = post.comment_set.all()
+#     context = {'post': post,
+#                'form': form,
+#                'comment_list': comment_list
+#
+#     }
+#     return render(request, 'blog/detail.html', context=context)
+
+
+# def archives(request, year, month):
+#     post_list = Post.objects.filter(create_time__year=year,
+#                                     create_time__month=month
+#                                     ).order_by('-create_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-create_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 1
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(create_time__year=year,
+                                                               create_time__month=month)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
+
